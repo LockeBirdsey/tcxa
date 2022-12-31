f8ca495f1b912f950f6c483e222996bfe25797ff
Locke Birdsey <locke.birdsey@gmail.com>
2022-12-31 10:30:20 +0200

    works 80% of what i need

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufWriter;
 
 use std::str;
 
@@ -19,7 +18,7 @@ fn main() {
     // load file
     let file_path = "test-mini.tcx";
     let mut file = match File::create("test-mini-out.tcx") {
-        Err(why) => panic!("couldn't create {}: {}","test-mini-out.tcx", why),
+        Err(why) => panic!("couldn't create {}: {}", "test-mini-out.tcx", why),
         Ok(file) => file,
     };
     let xml = fs::read_to_string(file_path).expect("File file_path could not be read");
@@ -28,7 +27,11 @@ fn main() {
     let mut reader = Reader::from_str(&xml);
     reader.trim_text(true);
     let mut writer = Writer::new(Cursor::new(Vec::new()));
+
+    // States
     let mut ignore: bool = false;
+    let mut trackpoint: bool = false;
+
     loop {
         match reader.read_event() {
             Ok(Event::Start(e)) if e.name().as_ref() == b"Position" => {
@@ -37,28 +40,17 @@ fn main() {
             Ok(Event::End(e)) if e.name().as_ref() == b"Position" => {
                 ignore = false;
             }
-            // Ok(Event::Start(e)) if e.name().as_ref() == b"this_tag" => {
-            //     // crates a new element ... alternatively we could reuse `e` by calling
-            //     // `e.into_owned()`
-            //     let mut elem = BytesStart::new("my_elem");
 
-            //     // collect existing attributes
-            //     elem.extend_attributes(e.attributes().map(|attr| attr.unwrap()));
-
-            //     // copy existing attributes, adds a new my-key="some value" attribute
-            //     elem.push_attribute(("my-key", "some value"));
-
-            //     // writes the event to the writer
-            //     assert!(writer.write_event(Event::Start(elem)).is_ok());
-            // }
-            // Ok(Event::End(e)) if e.name().as_ref() == b"this_tag" => {
-            //     assert!(writer
-            //         .write_event(Event::End(BytesEnd::new("my_elem")))
-            //         .is_ok());
-            // }
+            Ok(Event::Start(e)) if e.name().as_ref() == b"DistanceMeters" => {
+                ignore = true;
+            }
+            Ok(Event::End(e)) if e.name().as_ref() == b"DistanceMeters" => {
+                ignore = false;
+            }
             Ok(Event::Eof) => break,
-            // we can either move or borrow the event to write, depending on your use-case
+
             Ok(e) => {
+                if trackpoint {}
                 if !ignore {
                     assert!(writer.write_event(e).is_ok())
                 }
@@ -67,13 +59,13 @@ fn main() {
         }
     }
     let result = writer.into_inner().into_inner();
-    let s = match str::from_utf8(result.as_ref()) {
+    let _s = match str::from_utf8(result.as_ref()) {
         Ok(v) => v,
         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
     };
-    match file.write_all(result.as_ref()){
+    match file.write_all(result.as_ref()) {
         Err(why) => panic!("Couldn't write {}", why),
-        Ok(_) => println!("Written successfully")
+        Ok(_) => println!("Written successfully"),
     }
     // println!("result: {}", s);
 }
